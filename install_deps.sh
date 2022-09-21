88448c1dbeaaaebbd707134eb6f029aa04235591
pythonbrad <fomegnemeudje@outlook.com>
2022-09-21 17:19:53 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git clone https://github.com/osscameroon/broken_link_checker.git
-cd broken_link_checker
+git clone https://github.com/osscameroon/broken_link_checker.git blc
+cd blc
 git checkout patch
 pip install -r requirements.txt
