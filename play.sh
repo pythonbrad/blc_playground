61187bed58179d96bb5fedde35769d7e54afc3f0
pythonbrad <fomegnemeudje@outlook.com>
2022-09-21 17:32:04 +0100

    Update play.sh

--- play.sh
+++ play.sh
@@ -2,7 +2,7 @@
 
 error=0
 
-for i in $(python -m blc.blc -D -n -d .5 -b 5 | grep "\w* errors" -o | grep "[0-9]*" -o)
+for i in $(python -m blc.blc -D -n -d .5 -b 5 $HOST | grep "\w* errors" -o | grep "[0-9]*" -o)
 do
   if [$i != 0]
   then
