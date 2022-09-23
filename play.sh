4999a1516e5b577b9d1ee5ced96708a546ffea88
pythonbrad <fomegnemeudje@outlook.com>
2022-09-23 22:00:30 +0100

    Update play.sh

--- play.sh
+++ play.sh
@@ -2,7 +2,7 @@
 
 error=0
 
-output=$(python -m blc.blc -D -n -d .5 -b 5 $HOST)
+output=$(python -m blc.blc -n -d .5 -b 5 $HOST)
 
 for i in $(echo "$output" | grep "\w* errors" -o | grep "[0-9]*" -o)
 do
